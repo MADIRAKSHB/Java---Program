f99e5761102263e90f701e56f1f14341aa970959
MADIRAKSHB <mathi13sb@gmail.com>
2026-04-29 10:09:31 +0530

    java
    Signed-off-by: MADIRAKSHB <mathi13sb@gmail.com>

---
+++ stringliteral.java
@@ -0,0 +1,11 @@
+ class stringliteral{
+    public static void main(String[] args) {
+        String s="abc";
+        String s1="bcd";
+        System.out.println(s==s1);
+
+
+        
+
+    }
+}
---
+++ task.java
@@ -0,0 +1,8 @@
+ class task{
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a++ + ++a + --a);
+        
+
+    }
+}
