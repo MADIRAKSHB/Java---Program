dc93afcde92e3b47926508bea2284cd3f18504db
MADIRAKSHB <mathi13sb@gmail.com>
2026-04-30 09:45:56 +0530

    basic if statements

---
+++ passfail.java
@@ -0,0 +1,14 @@
+
+public class passfail {
+    public static void main(String[] args) {
+        int marks=45;
+        if(marks>=40){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+    }
+    
+
