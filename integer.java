48088e08163f78decdef032ea7e10d05ebe8b976
MADIRAKSHB <mathi13sb@gmail.com>
2026-04-30 09:48:25 +0530

    basic if conditions
    Signed-off-by: MADIRAKSHB <mathi13sb@gmail.com>

---
+++ integer.java
@@ -0,0 +1,14 @@
+
+ class integer {
+    public static void main(String[] args) {
+        int a=10;
+        if(a>0){
+            System.out.println(" is a positive number");
+        } else if(a<0){
+            System.out.println(" is a negative number");
+        } else {
+            System.out.println(" is zero");
+        }
+    }
+    
+}
