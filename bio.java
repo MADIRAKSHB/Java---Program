a9932790231214092aa0d44798a4f44e8f25670b
MADIRAKSHB <mathi13sb@gmail.com>
2026-05-01 10:01:18 +0530

    todo list
    Signed-off-by: MADIRAKSHB <mathi13sb@gmail.com>

--- bio.java
+++ bio.java
@@ -1,12 +1,11 @@
 
 import java.util.*;
-
 class bio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Enter your aage: ");
+        System.out.println("Enter your age: ");
         int age = sc.nextInt();
         System.out.println("Enter your phonenumber:");
         long phoneNumber = sc.nextLong();
